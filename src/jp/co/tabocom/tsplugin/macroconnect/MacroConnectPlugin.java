7b2c74e5a739eb8287ed26ff45f543ccc0cc5d01
turbou <turbou@i.softbank.jp>
2015-10-20 13:21:36 +0900

    bug fix.

--- src/jp/co/tabocom/tsplugin/macroconnect/MacroConnectPlugin.java
+++ src/jp/co/tabocom/tsplugin/macroconnect/MacroConnectPlugin.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import jp.co.tabocom.teratermstation.Main;
 import jp.co.tabocom.teratermstation.model.TargetNode;
 import jp.co.tabocom.teratermstation.plugin.TeraTermStationPlugin;
 import jp.co.tabocom.teratermstation.ui.action.TeraTermStationAction;
@@ -21,21 +23,36 @@ public class MacroConnectPlugin implements TeraTermStationPlugin {
     @Override
     public List<TeraTermStationAction> getActions(TargetNode node, Shell shell, ISelectionProvider selectionProvider) {
         List<TeraTermStationAction> list = new ArrayList<TeraTermStationAction>();
+
         FilenameFilter macroFilter = new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
-                System.out.println(name);
                 if (name.endsWith(".macro")) {
                     return true;
                 }
                 return false;
             }
         };
+
         Map<String, File> macroMap = new HashMap<String, File>();
         File targetFile = node.getFile();
-        for (int i = 1; i <= targetFile.toPath().getNameCount(); i++) {
-            Path path = targetFile.toPath().subpath(0, i);
-            File chkDir = path.toFile();
+
+        List<File> parentDirList = new ArrayList<File>();
+        Main main = (Main) shell.getData("main");
+        Path rootPath = main.getToolDefine().getRootDirPath();
+        File rootDir = rootPath.toFile();
+
+        File parentDir = targetFile.getParentFile();
+        while (parentDir != null) {
+            parentDirList.add(parentDir);
+            if (parentDir.getPath().equals(rootDir.getPath())) {
+                break;
+            }
+            parentDir = parentDir.getParentFile();
+        }
+
+        Collections.reverse(parentDirList);
+        for (File chkDir : parentDirList) {
             if (chkDir.isDirectory()) {
                 System.out.println("chkDir: " + chkDir.toString());
                 for (File file : chkDir.listFiles(macroFilter)) {
@@ -51,7 +68,6 @@ public class MacroConnectPlugin implements TeraTermStationPlugin {
 
     @Override
     public PreferencePage getPreferencePage() {
-        // TODO 自動生成されたメソッド・スタブ
         return null;
     }
 }
